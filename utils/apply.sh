2b4c2ba6b9eb83854e9614778deeae9d6f418767
Build Server <build@localhost>
2022-07-12 13:14:48 +0000

    utils: some fixes

--- utils/apply.sh
+++ utils/apply.sh
@@ -6,10 +6,11 @@ TOPDIR=$(pwd)
 if [ $# -gt 0 ]; then
 	CATEGORIES="$@"
 else
-	CATEGORIES="$(find -L $MY_PATH -mindepth 1 -maxdepth 1 -type d|sed s\|$MY_PATH/\|\|g|grep -vE '/debug$|/android-x86$|/security-patch-fix$|/feature-removal$')"
+	CATEGORIES="$(find -L $MY_PATH -mindepth 1 -maxdepth 1 -type d|sed s\|$MY_PATH/\|\|g|grep -vE 'debug|android-x86|security-patch-fix|feature-removal')"
 fi
 
 mkdir .repo/local_manifests || true
+rm patch-failed.txt || true
 
 for d in $CATEGORIES; do
 	if [ -f "$MY_PATH/$d/local_manifest.xml" ]; then
@@ -24,7 +25,10 @@ for d in $CATEGORIES; do
 	if [ -z "$SKIP_PATCH" ]; then
 	for r in `dirname $(find -L $MY_PATH/$d -name *.patch)|sort|uniq|sed "s|$MY_PATH/$d/||g"`; do
 		cd $r
-		git am $MY_PATH/$d/$r/*.patch
+		if ! git am $MY_PATH/$d/$r/*.patch; then
+			git am --abort || true
+			echo "${d} ${r}" >> $TOPDIR/patch-failed.txt
+		fi
 		cd $TOPDIR
 	done # repo
 	fi
